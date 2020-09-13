13721274362715be4fffab82fda9ca1f70c8f370
Petrus97 <ale199765@live.it>
2020-09-13 18:57:58 +0200

    updated results

--- test.py
+++ test.py
@@ -2,71 +2,78 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sort_performance as sp
 
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
 
 def test_sorted():
-    df = pd.DataFrame(columns=['Size', 'MergeSort', 'InsertionSort'])
-    df['Size'] = [100, 500, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 35000, 50000]
-    merge_time = sp.mergesort_performance_sorted(df['Size'])
+    df = pd.DataFrame(columns=['Dimensione array', 'MergeSort', 'InsertionSort'])
+    df['Dimensione array'] = [100, 500, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 35000, 50000]
+    merge_time = sp.mergesort_performance_sorted(df['Dimensione array'])
     df['MergeSort'] = merge_time
-    ins_time = sp.insertionsort_performance_sorted(df['Size'])
+    ins_time = sp.insertionsort_performance_sorted(df['Dimensione array'])
     df['InsertionSort'] = ins_time
-    print(df)
-    plt.xlabel("Array dimension")
-    plt.ylabel("Time to sorting")
-    plt.plot(df['Size'], merge_time, marker="o")
-    plt.plot(df['Size'], ins_time, marker="o")
+    logging.info(df)
+    # print(df)
+    # plt.xlabel("Array dimension")
+    # plt.ylabel("Time to sorting")
+    plt.plot(df['Dimensione array'], merge_time, marker="o")
+    plt.plot(df['Dimensione array'], ins_time, marker="o")
     plt.legend(['mergesort', 'insertionsort'], loc='upper left')
-    plt.title("Sorted Array")
+    plt.title("Array ordinato")
     plt.suptitle("InsertionSort vs MergeSort")
     plt.savefig('figures/sorted.png')
     plt.clf()
-    df.to_latex("results/sorted.tex")
+    df.to_latex(buf="results/sorted.tex", index=False, column_format='|l|l|l|', bold_rows=True)
 
 
 def test_random():
-    df = pd.DataFrame(columns=['Size', 'MergeSort', 'InsertionSort'])
-    df['Size'] = [100, 500, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 35000, 50000]
-    merge_time = sp.mergesort_performance_random(df['Size'])
+    df = pd.DataFrame(columns=['Dimensione array', 'MergeSort', 'InsertionSort'])
+    df['Dimensione array'] = [100, 500, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 35000, 50000]
+    merge_time = sp.mergesort_performance_random(df['Dimensione array'])
     df['MergeSort'] = merge_time
-    ins_time = sp.insertionsort_performance_random(df['Size'])
+    ins_time = sp.insertionsort_performance_random(df['Dimensione array'])
     df['InsertionSort'] = ins_time
     print(df)
-    plt.xlabel("Array dimension")
-    plt.ylabel("Time to sorting(ms)")
-    plt.plot(df['Size'], merge_time, marker="o")
-    plt.plot(df['Size'], ins_time, marker="o")
+    # plt.xlabel("Array dimension")
+    # plt.ylabel("Time to sorting(ms)")
+    plt.plot(df['Dimensione array'], merge_time, marker="o")
+    plt.plot(df['Dimensione array'], ins_time, marker="o")
     plt.legend(['mergesort', 'insertionsort'], loc='upper left')
-    plt.title("Random Array")
+    plt.title("Array randomico")
     plt.suptitle("InsertionSort vs MergeSort")
     plt.savefig('figures/random.png')
     plt.clf()
-    df.to_latex("results/random.tex")
+    df.to_latex("results/random.tex", index=False, column_format='|l|l|l|', bold_rows=True)
 
 
 def test_reverse():
-    df = pd.DataFrame(columns=['Size', 'MergeSort', 'InsertionSort'])
-    df['Size'] = [100, 500, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 35000, 50000]
-    merge_time = sp.mergesort_performance_reverse(df['Size'])
+    df = pd.DataFrame(columns=['Dimensione array', 'MergeSort', 'InsertionSort'])
+    df['Dimensione array'] = [100, 500, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 35000, 50000]
+    merge_time = sp.mergesort_performance_reverse(df['Dimensione array'])
     df['MergeSort'] = merge_time
-    ins_time = sp.insertionsort_performance_reverse(df['Size'])
+    ins_time = sp.insertionsort_performance_reverse(df['Dimensione array'])
     df['InsertionSort'] = ins_time
     print(df)
-    plt.xlabel("Array dimension")
-    plt.ylabel("Time to sorting(ms)")
-    plt.plot(df['Size'], merge_time, marker="o")
-    plt.plot(df['Size'], ins_time, marker="o")
+    # plt.xlabel("Array dimension")
+    # plt.ylabel("Time to sorting(ms)")
+    plt.plot(df['Dimensione array'], merge_time, marker="o")
+    plt.plot(df['Dimensione array'], ins_time, marker="o")
     plt.legend(['mergesort', 'insertionsort'], loc='upper left')
-    plt.title("Reverse Array")
+    plt.title("Array ordinato al contrario")
     plt.suptitle("InsertionSort vs MergeSort")
     plt.savefig('figures/reverse.png')
     plt.clf()
-    df.to_latex("results/reverse.tex")
+    df.to_latex("results/reverse.tex", index=False, column_format='|l|l|l|', bold_rows=True)
 
 
 def test_all():
     print("SORTED")
     test_sorted()
-    print("RANDOM")
-    test_random()
-    print("REVERSE")
-    test_reverse()
+    # print("RANDOM")
+    # test_random()
+    # print("REVERSE")
+    # test_reverse()
